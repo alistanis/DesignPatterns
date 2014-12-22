e74eadea55c33833a4fd5a3c1d101765d36ce1fa
Chris Cooper <christopher.michael.cooper@gmail.com>
2014-12-22 04:51:41 -0500

    update version number

--- lib/Patterns/version.rb
+++ lib/Patterns/version.rb
@@ -1,4 +1,4 @@
 module Patterns
   # The version of the Gem. Incremented with each new pattern implemented. When all are complete, the major version number will switch to 1.
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
