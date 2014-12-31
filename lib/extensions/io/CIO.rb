0d7c787eb596d01d10251424e361332f4154599d
Chris Cooper <christopher.michael.cooper@gmail.com>
2014-12-31 18:19:20 -0500

    adds documentation for module

---
+++ lib/extensions/io/CIO.rb
@@ -0,0 +1,4 @@
+# For documentation - C library that stands either for C Input/Output or Cooplib Input/Output. Contains functions for IO that are much faster than the ruby standard functions
+module CIO
+
+end
