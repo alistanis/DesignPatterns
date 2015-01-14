b3c9f88aa29fa3a5efe3d1b557e75787c34f8d07
Chris Cooper <christopher.michael.cooper@gmail.com>
2015-01-14 02:00:51 -0500

    adds aggregate log for test and total test example/failure count

--- script/run_tests.rb
+++ script/run_tests.rb
@@ -38,6 +38,10 @@ class String
 
 end
 
+example_lines = []
+map = {'[32m' => '', '[0m' => '', '[31m' => '', '[36m#' => '', "\e" => ''}
+re = Regexp.union(map.keys)
+
 # captures the output using script, so ansi colors are kept and will output correctly
 test_files.each do |file|
   out_file = File.expand_path('../../test', __FILE__) + '/logs/' + File.basename(file, '.rb') + '.log'
@@ -52,6 +56,9 @@ test_files.each do |file|
       readable.each do |io|
         until (str = io.gets).nil? do
           printf str
+          if str.include?('examples')
+            example_lines << str
+          end
           output_log << str
         end
       end
@@ -59,10 +66,39 @@ test_files.each do |file|
       puts e.backtrace
     end
   }
-  map = {'[32m' => '', '[0m' => '', '[31m' => '', '[36m#' => '' }
-  re = Regexp.union(map.keys)
+  # the next few lines remove ansi colors from text so the log doesn't look weird
+
   output = output_log.join('').gsub(re, map)
   Logger.instance.info output, out_file
   total_run_time = Time.now - start_time
   puts "\nTotal test run time: #{total_run_time} seconds"
 end
+
+total_examples = 0
+total_failures = 0
+
+example_lines.map!{|line| line.gsub!(re, map)}
+
+example_lines.each do |line|
+  split_lines = line.split(', ')
+  split_lines.each do |split_line|
+    if split_line.include?('examples')
+      total_examples += split_line.split(' ')[0].to_i
+    elsif split_line.include?('failures')
+      total_failures += split_line.split(' ')[0].to_i
+    end
+  end
+end
+
+out_file = File.expand_path('../../test', __FILE__) + '/logs/aggregate.log'
+if total_failures == 0
+  output = "#{total_examples} examples run, with 0 failures!"
+  puts output.green
+  Logger.instance.info output, out_file
+else
+  output = "#{total_examples} examples run, with #{total_failures} failures."
+  puts output.red
+  Logger.instance.info output, out_file
+end
+
+
